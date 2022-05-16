770a94b96d934a3bc4d2c7ff0b8324f1eb543171
onelouis <70702030+onelouis@users.noreply.github.com>
2022-05-16 13:00:59 -0300

    Titulo com problemas

--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -3,8 +3,9 @@ import React from "react";
 
 function header() {
     return ( 
-    <div className="glass">
+        <div>
         <header className="headers">
+            <div className="glass">
                 <div className="toolbar">
                     <nav>
                         <img src="/imagens/logo.png" alt="logo"/>
@@ -18,7 +19,14 @@ function header() {
                         <p> LOGIN</p>
                     </nav>
                 </div>
+            </div> 
+            <div className="Titulo texto-central">
+                <p className="p">Caminhos do </p>
+                
+                <p className="p1">Planalto Central</p>
+            </div>   
          </header>
+           
     </div>
     );
 }
